b147de89b785b0a3b87a1886f48752b64c0c34f4
Saryio <dev.saryio@gmail.com>
2022-07-21 00:16:06 -0300

    feat: add user

---
+++ src/modules/user/dto/create-user.dto.ts
@@ -0,0 +1,4 @@
+export class CreateUserDto {
+  wallet_address: string;
+  nickname: string;
+}
---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,31 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserRepository } from './user.repository';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @Inject(UserRepository)
+    private readonly userRepository: UserRepository,
+  ) {}
+  async create(createUserDto: CreateUserDto) {
+    return await this.userRepository.createUser(createUserDto);
+  }
+
+  async findAll() {
+    return await this.userRepository.users({});
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
